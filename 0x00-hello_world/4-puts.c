1badaaf7343f83e7a294260adc56017b123d8e8a
Paola <gracefulnes7@gmail.com>
2022-12-09 04:01:08 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-
-int main(void){
-	char Mystr[]="\"Programming is like building a multilingual puzzle";
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ * This program print some text by using puts
+ */
+int main(void) 
+{
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 	puts(Mystr);
 	return (0);
 }
