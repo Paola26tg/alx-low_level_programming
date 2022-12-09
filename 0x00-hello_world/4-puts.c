64ab1ebc39feb47eae53245b345b260798ed24ad
Paola <gracefulnes7@gmail.com>
2022-12-09 04:12:31 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,8 @@
  * This program print some text by using puts
  */
 int main(void)
-{
-  	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+{	
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 	puts(Mystr);
 	return (0);
 
