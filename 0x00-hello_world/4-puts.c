d8318a354b3556724dc64689373c3666b8a79519
Paola <gracefulnes7@gmail.com>
2022-12-09 04:10:32 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,10 @@
  * Return: Always 0 (Success)
  * This program print some text by using puts
  */
-int main(void) 
+int main(void)
 {
-	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+  	char Mystr[] = "\"Programming is like building a multilingual puzzle";
 	puts(Mystr);
 	return (0);
+
 }
