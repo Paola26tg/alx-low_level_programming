b6b45b7b209980f2f82d1ce00203a1c8066c5e80
Paola <gracefulnes7@gmail.com>
2022-12-10 03:05:42 -0800

    Different combination of three digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -2,7 +2,6 @@
 
 /**
  * main - Entry point prints combination of three digits
- *
  * Return: Always 0 (success)
  *
  */
@@ -10,15 +9,7 @@ int main(void)
 {
 	int i;
 
-	int j = 0;
-
-	int l = 0;
-
-	int k = 1;
-
-	int m = 1;
-
-	int n = 1;
+	int j = 0, l = 0, k = 1, m = 1, n = 1;
 
 	for (i = 0; i < 8; i++)
 	{
