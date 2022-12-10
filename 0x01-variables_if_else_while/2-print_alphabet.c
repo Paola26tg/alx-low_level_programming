c49d97f786ee559544fc57e3bbe0048f98a732de
Paola <gracefulnes7@gmail.com>
2022-12-09 18:03:43 -0800

    Alphabets in lower case

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point prints the alphabets in lower case
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	char mystr[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+		putchar(mystr[i]);
+	return (0);
+}
