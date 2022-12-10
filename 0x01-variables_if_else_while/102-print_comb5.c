443d392e13278cd986c24e810b8c2716cde19e63
Paola <gracefulnes7@gmail.com>
2022-12-10 03:59:18 -0800

    Different combination of two two-digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+/**
+ * main - Entry point prints combination of two two-digits numbers
+ *
+ * Return: Always 0 (success)
+ *
+ */
+int main(void)
+{
+	int i, j = 0, k = 0;
+
+	int l = 1, m = 0, n = 1, o = 2;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 8; j++)
+		{
+			for (; k <= 9; k++)
+			{
+				for (; l <=9; l++)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(' ');
+					putchar(k + '0');
+					putchar(l + '0');
+					if (j < 8)
+					{
+						putchar(',');
+						putchar(' ');
+					
+					}
+				}
+				l = m;
+				m++;
+			}
+			k = n;
+			n++;
+		}
+		j = o;
+		o++;
+	}
+	putchar('\n');	
+	return (0);
+}
