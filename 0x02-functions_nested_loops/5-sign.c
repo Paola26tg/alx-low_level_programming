d34dc24716afec4bb5da9e1a70b11137809e7147
Paola <gracefulnes7@gmail.com>
2022-12-13 11:48:35 -0800

    Check if character is letter

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_sign - prints a sign of a number
+ *
+ * Return: 1 if number is positive, 0 if is zero , -1 otherwise
+ * @n: number to prints sign
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	} else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
+	
