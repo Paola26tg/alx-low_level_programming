995c3f09268051c2f67938ca5fdf140beda9dbc2
Paola <gracefulnes7@gmail.com>
2022-12-13 14:34:36 -0800

    Prints 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * times_table - Prints 9 times table
+ *
+ */
+void time_table(void)
+{
+	int i = 0, j, k;
+
+	while (i <= 9)
+	{
+		_putchar('0');
+		for (j = 1; j <= 9; j++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			k = i * j;
+			if (k <= 9)
+				_putchar(' ');
+			else
+				_putchar(k / 10 + '0');
+			_putchar(k % 10 + '0');
+		}
+		i++;
+	}
+}
