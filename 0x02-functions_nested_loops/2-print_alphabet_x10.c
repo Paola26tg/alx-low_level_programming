56f2530dfeb21b10f9138ee935b07594994ce96c
Paola <gracefulnes7@gmail.com>
2022-12-13 10:09:46 -0800

    Prints 10 times alphabets in lowercase

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times of alphabets in lowercase
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+
+	char j;
+
+	while (i <= 10)
+	{
+		for (j = 'a'; j <= 'z'; j++)
+			_putchar(j);
+		_putchar('\n');
+	}
+
