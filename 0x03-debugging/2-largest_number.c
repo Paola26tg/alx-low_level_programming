878169520af32121e8d5502b331e19aedf7666c8
Paola <gracefulnes7@gmail.com>
2022-12-15 13:19:06 -0800

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,6 +7,7 @@
  * @c: third integer
  * Return: largest number
  */
+
 int largest_number(int a, int b, int c)
 {
 	int largest;
@@ -14,12 +15,15 @@ int largest_number(int a, int b, int c)
 	if (a >= b && b >= c)
 	{
 		largest = a;
-	} else if (b >= a && a >= c)
+	}
+	else if (b >= a && a >= c)
 	{
 		largest = b;
-	} else
+	}
+	else
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
