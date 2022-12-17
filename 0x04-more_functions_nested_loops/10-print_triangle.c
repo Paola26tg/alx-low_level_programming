94aba00d5e16614e3765e22b20a71084b21a538d
Paola <gracefulnes7@gmail.com>
2022-12-16 18:48:30 -0800

    Draw a tringle in terminale

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - Draw a triangle
+ * @size: the size of triangle to draw
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < size ; i++)
+		{
+			for (j = size - i; j > 1; j--)
+			{
+				_putchar(' ');
+			}
+			for (k = 0; k <= i; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
