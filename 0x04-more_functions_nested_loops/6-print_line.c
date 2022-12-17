0224b590b6578e443d636fbc32d47f9383ba80e7
Paola <gracefulnes7@gmail.com>
2022-12-16 17:21:30 -0800

    Draw straigth line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a straigth line in the terminal
+ * @n: number of line to draw
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
