ed063f2211ce25b38a69e3c795d484f1fc3516e1
Paola <gracefulnes7@gmail.com>
2022-12-19 00:25:56 -0800

    find print largest factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+/**
+ * main - finds and prints the largest prime factor of the number 612852475143, followed by a new line.
+ * Return: Always 0
+ *
+ */
+int main(void)
+{
+	long i, max;
+	long num = 612852475143;
+
+	for (i = 1; i <= sqrt(num); i++)
+	{
+		if(num % i == 0)
+		{
+			max = num / i;
+		}
+	}
+	printf("%d",max);
+}
