a225c5445836890bc37281cc68846ecdcc60443c
Paola <gracefulnes7@gmail.com>
2022-12-19 15:38:16 -0800

    Prints string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		_putchar(s[i]);
+	_putchar('\n');
+}
