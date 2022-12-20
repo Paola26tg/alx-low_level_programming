e0decbbc9f42a0bd152259e6bd52280f3011aa35
Paola <gracefulnes7@gmail.com>
2022-12-20 03:51:17 -0800

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strcpy - copies the string pointed to by src
+ * @dest: pointed to
+ * @src: string pointed
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char ptr;
+
+	ptr = *src;
+	dest = ptr;
+	return(dest);
+}
