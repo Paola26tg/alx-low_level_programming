60d45c306b95bd6611c77d2decf27a72b2353657
Paola <gracefulnes7@gmail.com>
2022-12-20 05:06:23 -0800

    Generate password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,10 +8,11 @@
  */
 int main(void)
 {
-	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$%^&*()_- +=QWERTYUIOPASDFGHJKLZXCVBNM[]{};':\"<>,.?/\|";
+	int i;
+	char list[] = "1234567890qwertyuiopasdfghjklzxcvbnm!@#$^&*?QWERTYUIOPASDFGHJKLZXCVBNM";
 
 	srand(time(NULL));
-	for(int i = 0; i < 10; i++)
+	for(i = 0; i < 10; i++)
 	{
 		printf("%c", list[rand() % (sizeof list - 1)]);
 	}
