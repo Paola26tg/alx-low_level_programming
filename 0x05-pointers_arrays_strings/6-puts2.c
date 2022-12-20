364bffb9465c37fe5f20226d66d49b1d790dbde3
Paola <gracefulnes7@gmail.com>
2022-12-20 02:08:20 -0800

    Prints other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints every character of the string starting by the first
+ *
+ * @str: string to be prints
+ */
+void puts2(char *str)
+{
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+		i++;
+	_putchar(s[j]);
+	for (; j < i; j++)
+	{
+		_putchar(s[j+1]);
+	}
+	_putchar('\n');
+}
