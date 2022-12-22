beac250db2451a98a83a02eab6c68eebace8687c
Paola <gracefulnes7@gmail.com>
2022-12-22 08:04:49 -0800

    copies strings

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strncpy - copies strings
+ *
+ * @dest: destination string
+ * @src: source string
+ * @n: integer 
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, j = 0;
+
+	while (dest[i])
+		i++;
+
+	while (j < n && src[j])
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
