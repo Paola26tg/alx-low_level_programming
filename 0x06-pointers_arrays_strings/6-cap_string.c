eacb09426a0d72631e6d39d7e2e24275323dbee0
Paola <gracefulnes7@gmail.com>
2022-12-22 15:27:07 -0800

    capitalize words

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * cap_string - capitalize all words in string
+ * @a: string to capitalize
+ * Return: char
+ */
+char *cap_string(char *a)
+{
+	int s = 0, i;
+	int j = 13;
+	char spc[] = {32, '\t', '\n', 44, ';', 46, '!', '?', '"', '(', ')', '{', '}'};
+
+	while (a[s])
+	{
+		i = 0;
+
+		while (i < j)
+		{
+			if ((s == 0 || a[s - 1] == spc[i]) && (a[s] >= 97 && a[s] <= 122))
+				a[s] = a[s] - 32;
+
+			i++;
+		}
+
+		s++;
+	}
+
+	return (a);
