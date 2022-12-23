4996dba386104d7b53432c0c3b89b32ec8d26239
Paola <gracefulnes7@gmail.com>
2022-12-22 16:54:27 -0800

    Print buffer

---
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * infinite_add - adds two numbers
+ * @n1: first number
+ * @n2: second number
+ * @r: result
+ * @size_r: buffer size
+ * Return: char
+ */
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+{
+	int i = 0;
+	return (r);
+}
---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_buffer - Prints a buffer
+ * @b: buffer
+ * @size: size of buffer
+ * Return: nothing
+ */
+void print_buffer(char *b, int size)
+{
+	int i = 0;
+}
