3a07f4a2a6aab63fdcc688f7306e29ef812d045a
Paola <gracefulnes7@gmail.com>
2022-12-22 16:10:58 -0800

    Prints number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_number - Prints integer
+ * @n: integer to print
+ */
+void print_number(int n)
+{
+	unsigned int i = n;
+
+	if (n < 0)
+	{
+		n = n * -1;
+		i = n;
+		_putchar('-');
+	}
+	i = i / 10;
+
+	if (i != 0)
+		print_number(i);
+	_putchar((unsigned int) n % 10 + '0');
+}
