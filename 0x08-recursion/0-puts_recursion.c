6ac9b553eb71ddac1e60906d64db448f83032138
Paola <gracefulnes7@gmail.com>
2023-01-05 15:51:49 -0800

    C- recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts-recursion - prints a string
+ * @s: string to print
+ *
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+	_putchar('\n');
+		return;
+	}
+	_putchar(s[0]);
+	_puts-recursion(s + 1);
+}
