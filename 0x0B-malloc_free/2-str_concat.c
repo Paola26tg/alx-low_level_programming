0dd7a4db82ba957cac2450d419481a4164ee1136
Paola <gracefulnes7@gmail.com>
2023-01-10 16:01:14 -0800

    Concatanate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,14 +17,14 @@ char *str_concat(char *s1, char *s2)
 	}
 	if (s2 == NULL)
 		s2 = "";
-	for (i = 0, s1[i] || s2[i], i++)
+	for (i = 0; s1[i] || s2[i]; i++)
 		j++;
 	s = malloc(sizeof(char) * j);
 	if (s == NULL)
 		return (NULL);
-	for (i = 0, s1[i], i++)
+	for (i = 0; s1[i]; i++)
 		s[k++] = s1[i];
-	for (i = 0, s2[i], i++)
+	for (i = 0; s2[i]; i++)
 		s[k++] = s2[i];
 	return (s);
 }
